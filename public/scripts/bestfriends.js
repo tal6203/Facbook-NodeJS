7c954076e69e594129f5cda723bc73d76a53ca42
Tal Abutbul <112417918+tal6203@users.noreply.github.com>
2023-12-19 04:56:18 +0200

    Update bestfriends.js

--- public/scripts/bestfriends.js
+++ public/scripts/bestfriends.js
@@ -242,7 +242,7 @@ function filterUsername(searchValue) {
 
 
 function getAllFriends() {
-      fetch(`https://facbook-node-740n59hru-tal6203.vercel.app/friends/${user.id}`)
+      fetch(`https://facbook-nodejs.onrender.com/friends/${user.id}`)
       .then((res) =>  {
     if (res.status === 204) {
       return {}; // Return an empty object
