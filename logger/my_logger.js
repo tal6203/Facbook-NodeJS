0380449b0fd9cd26dabbb1346619747637447a0f
Tal Abutbul <112417918+tal6203@users.noreply.github.com>
2023-12-19 03:02:33 +0200

    Update my_logger.js

--- logger/my_logger.js
+++ logger/my_logger.js
@@ -1,27 +1,27 @@
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, prettyPrint } = format;
 
-// const currentDate = new Date();
-// const year = currentDate.getFullYear();
-// const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-// const day = String(currentDate.getDate()).padStart(2, '0');
-// const formattedDate = `${year}-${month}-${day}`;
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+const day = String(currentDate.getDate()).padStart(2, '0');
+const formattedDate = `${year}-${month}-${day}`;
 
-// const logger = createLogger({
-//     level: 'debug',
-//     format: combine(
-//         label({ label: 'Logger!' }),
-//         timestamp({
-//             format: 'DD/MM/YYYY HH:mm'
-//         }),
-//         prettyPrint()
-//     ),
-//     transports: [
-//         new transports.Console(),
-//         new transports.File({
-//             filename: `./logs/${formattedDate}.log`
-//         })
-//     ]
-// });
+const logger = createLogger({
+    level: 'debug',
+    format: combine(
+        label({ label: 'Logger!' }),
+        timestamp({
+            format: 'DD/MM/YYYY HH:mm'
+        }),
+        prettyPrint()
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.File({
+            filename: `./logs/${formattedDate}.log`
+        })
+    ]
+});
 
 module.exports = logger;;
