b4d9498a9275fcb60289044f738a8f901d267d5f
Tal Abutbul <112417918+tal6203@users.noreply.github.com>
2023-12-19 03:19:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 // middleware
 app.use(express.static('public'));
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public'));
 app.use(express.json());
 app.use(cookieParser());
 
