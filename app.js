e5ac7ad791f573cf8c4689ba47e28f68f9efb8aa
Tal Abutbul <112417918+tal6203@users.noreply.github.com>
2023-12-19 04:14:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ const app = express();
 app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('public', path.join(__dirname, 'public'));
+app.use(cors({
+  origin: 'https://facbook-node-740n59hru-tal6203.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
