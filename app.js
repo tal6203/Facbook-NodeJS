2e9de8d933e1ab96fd243d1d95a05c7efb51503f
Tal Abutbul <112417918+tal6203@users.noreply.github.com>
2023-12-21 07:16:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,13 +14,12 @@ const app = express();
 
 
 
-const corsOptions = {
-  origin: 'https://facbook-node-js.vercel.app/',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://facbook-node-js.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 
 // middleware
