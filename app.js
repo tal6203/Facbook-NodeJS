859fff796005b8c1f3b0db57ea853f1588e1c9d7
Tal Abutbul <112417918+tal6203@users.noreply.github.com>
2023-12-21 07:37:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ const path = require('path');
 const app = express();
 
 
-
+app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+})
 
 
 
