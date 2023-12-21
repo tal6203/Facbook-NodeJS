3ce95c9fa376915bf8425dcced23ef2b8f4a0a3c
Tal Abutbul <112417918+tal6203@users.noreply.github.com>
2023-12-21 07:24:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,12 +14,7 @@ const app = express();
 
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://facbook-node-js.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+
 
 
 // middleware
