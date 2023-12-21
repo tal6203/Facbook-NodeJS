667551da12a7c079b2d476fcbe27f4777db47759
Tal Abutbul <112417918+tal6203@users.noreply.github.com>
2023-12-21 07:25:38 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const controller = require('../controllers/controller')
-const { ToBestFriends, corsServer } = require('../middleware/middleware');
+const { ToBestFriends } = require('../middleware/middleware');
 
 
 
@@ -12,12 +12,12 @@ router.post('/signup', controller.signup_post);
 router.get('/login', controller.login_get);
 router.post('/login', controller.login_post);
 router.get('/logout', controller.logout_get);
-router.get('/bestfriends', ToBestFriends,corsServer, controller.best_friends_get_page);
-router.get('/friends/:userId',corsServer ,controller.best_friends_get_friendlist);
-router.get('/friends/:userId/:friendId',corsServer,controller.get_friend_by_id);
-router.post('/friends/:userId',corsServer ,controller.add_new_friend);
-router.delete('/friends/:userId/:friendId', corsServer,controller.delete_best_friend);
-router.put('/friends/:userId/:friendId',corsServer,controller.update_best_friend);
+router.get('/bestfriends', ToBestFriends, controller.best_friends_get_page);
+router.get('/friends/:userId' ,controller.best_friends_get_friendlist);
+router.get('/friends/:userId/:friendId',controller.get_friend_by_id);
+router.post('/friends/:userId' ,controller.add_new_friend);
+router.delete('/friends/:userId/:friendId',controller.delete_best_friend);
+router.put('/friends/:userId/:friendId',controller.update_best_friend);
 
 module.exports = router;
 
